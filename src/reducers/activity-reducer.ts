0f681808b5b8b7a9d6f52cc5fb2cddcf6a485b84
Franco Armando <francoarmando1911@gmail.com>
2024-11-01 17:10:44 -0300

    actualizando actividades

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -20,13 +20,20 @@ export const activityReducer = (
     action: ActivityActions
 ) => {
     if(action.type === 'save-activity'){
-        //Manejo de logica para controlar el state
-        console.log(action.payload.newActivity)
-    
-    
+
+        let updatedActivities : Activity[] = []
+
+        if(state.activeId){
+            updatedActivities = state.activities.map( activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+
+        } else{
+            updatedActivities = [...state.activities, action.payload.newActivity]
+        }
+
         return {
             ...state,
-            activities: [...state.activities, action.payload.newActivity]
+            activities: updatedActivities,
+            activeId: ''
         }
 
     }
