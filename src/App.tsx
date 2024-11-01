0bb3bb226fd6bcfea03a5f2de32242059ea93ac2
Franco Armando <francoarmando1911@gmail.com>
2024-11-01 16:40:45 -0300

    Agregando estilos a ActivityList

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import './index.css'
 import Form from "./components/Form"
 import { useReducer } from 'react'
 import { activityReducer, initialState } from './reducers/activity-reducer'
+import ActivityList from './components/ActivityList'
 
 function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initialState)
+
+  
 
   return (
     <>
@@ -24,6 +27,13 @@ function App() {
             dispatch={dispatch}
           />
         </div>
+      </section>
+
+      <section className='p-10 mx-auto max-w-4xl'>
+        <ActivityList
+          activities={state.activities}
+        />
+
       </section>
     </>
   )
