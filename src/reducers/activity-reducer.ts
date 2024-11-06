d319185f250a6b8caee3db7aac4bfdcbf412a252
Franco Armando <francoarmando1911@gmail.com>
2024-11-06 16:57:36 -0300

    configuracion boton eliminar

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,7 +2,9 @@ import { Activity } from "../types"
 
 export type ActivityActions = | 
     { type: 'save-activity', payload: {newActivity : Activity} } |
-    { type: 'save-activeId', payload: { id: Activity['id'] } }
+    { type: 'save-activeId', payload: { id: Activity['id'] } } |
+    { type: 'delete-activity', payload: { id: Activity['id'] } }
+
 
 export type ActivityState = {
     activities: Activity[],
@@ -42,6 +44,13 @@ export const activityReducer = (
         return{
             ...state,
             activeId: action.payload.id
+        }
+    }
+
+    if(action.type === 'delete-activity'){
+        return {
+            ...state,
+            activities: state.activities.filter(activity => activity.id !== action.payload.id)
         }
     }
 
